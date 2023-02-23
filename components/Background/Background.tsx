7e5f630827da4223f23797b4c95238b1644e3d2f
Austin Ftacnik <austin@ftacnik.com>
2023-02-23 03:11:53 -0800

    feat(background): rotate background on scroll

--- components/Background/Background.tsx
+++ components/Background/Background.tsx
@@ -1,10 +1,16 @@
-import { FC, useEffect, useRef } from 'react';
-import { Canvas } from '@react-three/fiber';
+import { FC, useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import useScrollPos from 'hooks/useScrollPos';
 
-const Box: FC<{}> = () => {
-  const ref = useRef(null);
+const Box: FC<{ scrollY: number | null }> = ({ scrollY }) => {
+  const ref = useRef<THREE.Mesh>(null!);
 
-  useEffect(() => {}, []);
+  useFrame(() => {
+    const rotate = scrollY ? scrollY * 0.01 : 0;
+    ref.current.rotation.x = rotate;
+    ref.current.rotation.y = rotate;
+    ref.current.rotation.z = rotate;
+  });
 
   return (
     <mesh ref={ref} scale={1}>
@@ -14,13 +20,18 @@ const Box: FC<{}> = () => {
   );
 };
 
-const Background: FC<{}> = () => (
-  <div className="fixed top-0 left-0 w-screen h-screen bg-slate-400 -z-10">
-    <Canvas>
-      <ambientLight />
-      <Box />
-    </Canvas>
-  </div>
-);
+const Background: FC<{}> = () => {
+  const scrollY = useScrollPos();
+
+  return (
+    <div className="fixed top-0 left-0 w-screen h-screen bg-slate-400 -z-10">
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box scrollY={scrollY} />
+      </Canvas>
+    </div>
+  );
+};
 
 export default Background;
