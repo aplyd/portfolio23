01380465f4c948d2edb4ad2109d9ff109492cc9e
Austin Ftacnik <austin@ftacnik.com>
2023-02-23 02:40:25 -0800

    refactor(hero): use flex instead of absolute positioning

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,9 +1,13 @@
 import { FC } from 'react';
 
 const Hero: FC<{}> = () => (
-  <div className="relative h-screen bg-emerald-300">
-    <h1 className="absolute inset-1/2">Logo</h1>
-    <p className="absolute inset-x-1/2 bottom-12">arrow down</p>
+  <div className="h-screen text-white border">
+    <div className="flex items-center justify-center bg-transparent h-4/5">
+      <h1 className="">Logo</h1>
+    </div>
+    <div className="flex items-center justify-center bg-transparent h-1/5">
+      <p className="">arrow down</p>
+    </div>
   </div>
 );
 
