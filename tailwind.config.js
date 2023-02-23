48d9e3e285e12fea46697815359b35f085e6e678
Austin Ftacnik <austin@ftacnik.com>
2023-02-23 02:39:34 -0800

    feat(tailwind): add negative z-index to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      zIndex: {
+        '-10': '-10',
+      },
+    },
   },
   plugins: [],
 };
